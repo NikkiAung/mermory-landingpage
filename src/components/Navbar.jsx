af9265fe2e6885438756eea04c84da43be999e82
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-04-30 20:31:15 -0700

    improving nav abit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,17 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center gap-8">
-      <button className="text-black navbar-text">Tools</button>
-      <button className="text-black navbar-text">Explore</button>
-      <div className="w-[400px]">
-        <div class="flex items-center bg-gray-100 rounded-lg px-4 py-2 shadow-sm">
+    <nav className="flex items-center gap-10">
+      <div className="flex gap-15 mr-20">
+        <button className="text-[var(--primary-text-color)] navbar-text w-[67px] h-[40px]">
+          Tools
+        </button>
+        <button className="text-[var(--primary-text-color)] navbar-text w-[86px] h-[40px]">
+          Explore
+        </button>
+      </div>
+      <div className="w-[477px] h-[40px]">
+        <div class="flex items-center bg-gray-100 rounded-lg px-4 py-2 shadow-sm ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -17,7 +23,7 @@ const Navbar = () => {
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="lucide lucide-search-icon lucide-search w-4 h-4 text-gray-400 mr-2"
+            class="lucide lucide-search-icon lucide-search w-5 h-5 text-gray-600 mr-2"
           >
             <circle cx="11" cy="11" r="8" />
             <path d="m21 21-4.3-4.3" />
